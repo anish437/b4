ca2dba83950ce8342b3c6e64769ce95a86e6e278
anish437 <yeturuanish179@gmail.com>
2025-07-28 19:48:27 +0530

    Create difference and quotient.cpp

---
+++ difference and quotient.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+int a,b,c,d;
+cout<<"enter the a and b";
+cin>>a>>b;
+c=a-b;
+cout<<"the differce is:";
+cout<<c << "\n";
+d=a/b;
+cout<<"the quotient is:";
+cout<<d;
+
+
+}
